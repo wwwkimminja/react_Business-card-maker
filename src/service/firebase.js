4c5433d4352a45fada49b34a18f97d3ca288db4f
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-08-05 23:40:00 +0900

    first commit

---
+++ src/service/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+
+
+const app = initializeApp(firebaseConfig);
