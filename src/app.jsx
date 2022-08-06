fcbb4bc5ce7a889a62535746b27142bbb4640eb9
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-08-07 01:22:15 +0900

    add router

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,18 @@
 import styles from './app.module.css';
 import Login from './components/login/login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Maker from './components/maker/maker';
 
 function App({ authService }) {
   return (
     <div className={styles.app}>
 
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Routes>
+          < Route exact path="/" element={<Login authService={authService} />} />
+          <Route exact path="/maker" element={<Maker authService={authService} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
